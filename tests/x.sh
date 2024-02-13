00d24e6f2bada9113c17fb544c88f56e9a5cba6b
don caldwell <doncaldwell@dfwcMBM1.local>
2024-02-13 15:23:15 -0500

    change test file change version change README.md

--- tests/x.sh
+++ tests/x.sh
@@ -2,23 +2,33 @@
 set -ex
 
 fredcategories --categories --file /tmp/categories.csv 
-fredcategories --series --categoryid 33500 --file /tmp/cseries33500.csv 
-fredcategories --observations --directory /tmp --categoryid 33500
-#fredcategories --observations --directory /tmp --seriesid
+fredcategories --series --categoryid 32455 --file /tmp/cseries32455.csv 
+fredcategories --observations --directory /tmp --categoryid 32455
+#    fredcategories --observations --directory /tmp --seriesid
+ls /private/tmp/[A-Z]*.csv | wc -l
+rm /private/tmp/[A-Z]*.csv
 
 
 fredreleases --releases --file /tmp/releases.csv
-fredreleases --series --releaseid 471 --file /tmp/rseries471.csv
-fredreleases --observations --directory /tmp --releaseid 471
-#fredreleases --observations --directory /tmp --seriesid 
+fredreleases --series --releaseid 9 --file /tmp/rseries9.csv
+fredreleases --observations --directory /tmp --releaseid 9
+#    fredreleases --observations --directory /tmp --seriesid 
+ls /private/tmp/[A-Z]*.csv | wc -l
+rm /private/tmp/[A-Z]*.csv
 
-
-fredtags --tags --file /tmp/tags.csv
-fredtags --series --tagname employment --file /tmp/tseriesemployment.csv
-fredtags --observations --directory /tmp --tagname employment
-#fredtags --observations --directory /tmp --seriesid 
 
 fredsources --sources --file /tmp/sources.csv
-fredsources --releases --file /tmp/sreleases.csv
-#fredsources --sources --releasid 471
+fredsources --releases --sourceid 69 --file /tmp/sreleases69.csv
+#    fredsources --releases --file /tmp/sreleases.csv
+#    fredsources --sources --directory /tmp
+fredsources --observations --sourceid 69
+#ls /private/tmp/[A-Z]*.csv | wc -l
+#rm /private/tmp/[A-Z]*.csv
+
+fredtags --tags --file /tmp/tags.csv
+fredtags --series --tagname price --file /tmp/tseriesprice.csv
+fredtags --observations --directory /tmp --tagname price
+#    fredtags --observations --directory /tmp --seriesid 
+ls /private/tmp/[A-Z]*.csv | wc -l
+rm /private/tmp/[A-Z]*.csv
 
