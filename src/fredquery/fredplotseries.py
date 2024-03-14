28a3a68bc1b27234e64fff373d577d1e23c7f859
don caldwell <doncaldwell@dfwcMBM1.local>
2024-03-14 18:02:40 -0400

    fredplotseries a tiny bit better

--- src/fredquery/fredplotseries.py
+++ src/fredquery/fredplotseries.py
@@ -5,6 +5,7 @@ import sys
 import argparse
 
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
@@ -46,25 +47,53 @@ class PlotSeries():
 
             oaa = self.observationsdict[k]
 
+            print('%s %d' % (k, len(oaa)), file=sys.stderr)
+
+            dates = [oaa[i][2] for i in range(len(oaa) )]
+            vals  = [oaa[i][3] for i in range(len(oaa) )]
+
             if 'dates' not in self.data.keys():
                 self.data['dates'] = dates
-            if 'values' not in self.data.keys():
-                self.data['values'] =[[oaa[i][3] for i in range(len(oaa) )]]
-            else:
-                self.data['values'].append( [oaa[i][3] for i in
-                              range(len(oaa) )] )
-
-            #dates = [oaa[i][2] for i in range(1, len(oaa) )]
-            #vals = [oaa[i][3] for i in range(1, len(oaa) )]
-            #if 'dates' not in self.data.keys():
-                #self.data['dates'] = dates
-            #self.data[sid] = vals
+            self.data[sid] = vals
 
     def composeplot(self):
+        self.fig = go.Figure()
+
+        units = None
+
+        for k in self.seriesdict.keys():
+            saa = self.seriesdict[k]
+            sid    = saa[1][0]
+            stitle = saa[1][3]
+            units  = saa[1][8]
+            self.fig.add_trace(go.Scatter(
+                x=self.data['dates'],
+                y=self.data[k],
+                name=stitle
+            ) )
+
+        self.fig.update_layout(
+            title='FRED Time Series',
+            yaxis_title=units,
+            xaxis_title='dates'
+        )
+
+        self.fig.show()
+
+
+
+
+    def oldcomposeplot(self):
         self.df = pd.DataFrame(self.data)
         fig = None
 
         for k in self.seriesdict.keys():
+            assert saa[0][0] == 'id'
+            assert saa[0][3] == 'title'
+            assert saa[0][8] == 'units'
+            sid    = saa[1][0]
+            stitle = saa[1][3]
+            units  = saa[1][8]
             if not fig:
                 fig = px.line(self.df, x=self.df['dates'], y=self.df[k])
             else:
